5ddcf422d10fb682d3d8cce239e966f32385fec0
sksinghcodes <73813862+sksinghcodes@users.noreply.github.com>
2020-11-04 14:05:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
+	$(".nav-toggle-button").click(function(){
+		$(".nav-toggle-button").toggleClass("active");
+		$("nav").slideToggle();
+		console.log("clicked");
+	});
 
+	console.log("connected");
+});
